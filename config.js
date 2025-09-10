2c066dd95d44931562db0d53f97c4038d043b252
Blueberry <ope480927@gmail.com>
2025-09-10 21:05:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ import settings from './settings.js';
 
 export default {
   // Bot configuration
-  prefix: '?',
-  ownerNumber: '2347049044897',
-  botName: '✦✦✦ 𝐇 𝐎 𝐑 𝐋 𝐀 𝐏 𝐎 𝐎 𝐊 𝐈 𝐄 ✦✦✦',
-  ownerName: '𝓗𝓞𝓡𝓛𝓐𝓟𝓞𝓞𝓚𝓘𝓔',
-  sessionId: 'HORLA-POOKIE-SESSION-ID',
+  prefix: '?'.
+  ownerNumber: '2349060936030',
+  botName: 'G.cupid🖤',
+  ownerName: 'Øpeyemi',
+  sessionId: 'Øpeyemi-SESSION-ID',
   BOOM_MESSAGE_LIMIT: 50,  
 
   // API configurations from settings
